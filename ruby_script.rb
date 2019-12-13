df2ffd4802eac507ac22ff0f730fba28fcbc3252
Student <example@example.com>
2019-12-12 16:23:38 -0800

    add ability to write to ruby_temp and gemfile

--- ruby_script.rb
+++ ruby_script.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
-require('./lib/ruby_project_creator.rb')
 require 'fileutils'
-require 'bundle'
+# require 'bundle'
+FileUtils.cd("/Users/Guest")
+FileUtils.cd("Desktop")
+# FileUtils.cd("..")
 
 puts "Enter directory name:"
 directory_name = gets.chomp
@@ -13,3 +15,17 @@ FileUtils.mkdir("spec")
 FileUtils.touch("lib/ruby_temp.rb")
 FileUtils.touch("spec/ruby_temp_spec.rb")
 FileUtils.touch("Gemfile")
+FileUtils.touch("README.md")
+ruby_temp = File.open('lib/ruby_temp.rb', 'w')
+puts "Enter a class name: "
+class_name = gets.chomp
+puts "Enter a method name: "
+method_name = gets.chomp
+ruby_temp.puts "class " + class_name +
+"\n" + "  def " + method_name +
+"\n" + "  #code goes here" +
+"\n" + "  end" +
+"\n" + "end"
+
+gemfile = File.open('Gemfile', 'w')
+gemfile.puts "source 'https://rubygems.org'" + "\n" + "\n" + "gem 'rspec'" + "\n" + "gem 'pry'" + "\n" + "gem 'bundle'"
