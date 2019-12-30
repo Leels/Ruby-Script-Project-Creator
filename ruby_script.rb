f5e3832c75aafda97d7fc127c6085119b5034448
Lela <smith.lelaa@gmail.com>
2019-12-29 18:54:11 -0800

    update script

--- ruby_script.rb
+++ ruby_script.rb
@@ -1,9 +1,7 @@
 #!/usr/bin/env ruby
 require 'fileutils'
-# require 'bundle'
-FileUtils.cd("/Users/Guest")
-FileUtils.cd("Desktop")
-# FileUtils.cd("..")
+
+FileUtils.cd("..")
 
 puts "Enter directory name:"
 directory_name = gets.chomp
@@ -12,19 +10,23 @@ FileUtils.mkdir(directory_name)
 FileUtils.cd(directory_name)
 FileUtils.mkdir("lib")
 FileUtils.mkdir("spec")
+FileUtils.mkdir("views")
+FileUtils.mkdir("public")
 FileUtils.touch("lib/ruby_temp.rb")
 FileUtils.touch("spec/ruby_temp_spec.rb")
+FileUtils.touch("app.rb")
 FileUtils.touch("Gemfile")
 FileUtils.touch("README.md")
 ruby_temp = File.open('lib/ruby_temp.rb', 'w')
 puts "Enter a class name: "
 class_name = gets.chomp
-puts "Enter a method name: "
-method_name = gets.chomp
 ruby_temp.puts "class " + class_name +
-"\n" + "  def " + method_name +
-"\n" + "  #code goes here" +
+"\n" + "  attr_accessor" +
+"\n"
+"\n" + "  def initialize()" +
+"\n" + "    @" +
 "\n" + "  end" +
+"\n"
 "\n" + "end"
 
 gemfile = File.open('Gemfile', 'w')
